6664439afcb1017303f31c48b1b03db36f507b02
C17YVONNE <c17yvonne@gmail.com>
2024-05-11 16:03:32 +0900

    JAVA2
    文字列の連結

---
+++ src/com/lemicode/JAVA2.java
@@ -0,0 +1,9 @@
+package com.lemicode;
+//2つの文字列変数を宣言し、それらを連結して出力するプログラムを書いてください。
+public class JAVA2 {
+	public static void main(String[] args) {
+		String part1 = "こんにちは";
+		String part2 = "世界";
+		System.out.println(part1 + part2);
+	}
+}
