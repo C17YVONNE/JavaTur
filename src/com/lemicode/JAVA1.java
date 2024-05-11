23a323a28145d04f86dbd874fa77406c419f8bc3
C17YVONNE <c17yvonne@gmail.com>
2024-05-11 15:32:46 +0900

    JAVA1
    基本的な整数の宣言と使用

--- src/com/lemicode/JAVA1.java
+++ src/com/lemicode/JAVA1.java
@@ -1,10 +1,11 @@
 package com.lemicode;
 
 public class JAVA1 {
-
+//整数変数を宣言し、その変数に値を代入し、その値を出力するプログラムを書いてください。
 	public static void main(String[] args) {
-
-		System.out.println(999);
+		int a;
+		a = 1;
+		System.out.println(a);
+		
 	}
-
 }
