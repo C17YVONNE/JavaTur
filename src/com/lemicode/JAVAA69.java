95f0f7e5e27f8de353a2648efab5d8900b14c566
C17YVONNE <c17yvonne@gmail.com>
2024-05-23 21:55:21 +0900

    JAVA69
    文字列のトリミング

---
+++ src/com/lemicode/JAVAA69.java
@@ -0,0 +1,17 @@
+package com.lemicode;
+/**
+ * 「文字列トリミング」クラス
+ */
+public class JAVAA69 {
+
+	public static void main(String[] args) {
+		// 入力文字列
+		String input = "   こんにちは、LemiCodeさん！   ";
+		
+		//トリミング文字列
+		String trimOutput = input.trim();
+		System.out.println("トリミング後の文字列: " + trimOutput);
+
+	}
+
+}
