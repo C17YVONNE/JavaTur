6725d37ef922b54f39e9726d034ab37b90340b2b
C17YVONNE <c17yvonne@gmail.com>
2024-05-24 16:18:31 +0900

    JAVA71
    文字列の長さ計算

---
+++ src/com/lemicode/JAVA71.java
@@ -0,0 +1,17 @@
+package com.lemicode;
+/**
+ * 「文字列長さ計算」クラス
+ */
+public class JAVA71 {
+
+	public static void main(String[] args) {
+		// 入力文字列
+		String input = "こんにちは";
+		
+		//文字列の長さのメソッド 
+		int inputLength = input.length();
+		System.out.println("文字列の長さ: " + inputLength);
+
+	}
+
+}
